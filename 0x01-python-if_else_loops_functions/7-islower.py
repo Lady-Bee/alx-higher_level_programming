025d0ae2ea5c67fc7571a1a7865c57f454d66747
Lady-Bee <bisimi16@gmail.com>
2024-01-13 05:12:30 -0800

    task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def islower(c):
+    """Checks for lowercase
+
+    Args:
+        c: a single character argument
+
+    Returns:
+        True: if c is lowercase
+        False: if c is not lowercase
+    """
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    return False
