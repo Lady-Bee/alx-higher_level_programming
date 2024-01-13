100243951609bccb964db858c8acf98348f62f20
Lady-Bee <bisimi16@gmail.com>
2024-01-13 05:26:59 -0800

    task 10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Adds two integers
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        the result of addition
+    """
+    return a + b
