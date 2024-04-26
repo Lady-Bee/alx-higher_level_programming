2de3c1ce7a1864cc5423695d9cb306fcc3f5e876
Lady-Bee <bisimi16@gmail.com>
2024-04-26 03:40:11 -0700

    python network 0 tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a script that sends a POST request to a certain
-# URL and displays the body response
+# sends a POST request and displays the body response
 curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
