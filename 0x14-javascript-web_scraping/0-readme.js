9dc78ff7a2eff9ddbe941f61a7a8b712a6856a66
Lady-Bee <bisimi16@gmail.com>
2024-05-23 02:24:47 -0700

    javascript-web scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Read from file
+
+const fsys = require('fs');
+fsys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
